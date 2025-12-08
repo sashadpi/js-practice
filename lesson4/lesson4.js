436e788f119b0b7b7d830b6d789545abd274901c
*Sasha* <*sashadpi68@gmail.com*>
2025-12-08 21:10:36 +0200

    Add TypeScript definitions and JavaScript implementation for Chalk library
    - Introduced TypeScript definitions for Chalk in `index.d.ts` to provide type safety and better developer experience.
    - Implemented the main Chalk functionality in `index.js`, allowing for styled console output.
    - Created utility functions in `utilities.js` for string manipulation related to styling.
    - Added vendor files for ANSI styles and color support detection.
    - Included browser compatibility for color support in `browser.js`.
    - Implemented support for various color levels and styles in the `supports-color` module.
    - Added example JavaScript code in `lesson4.js` demonstrating basic operations and type conversions.

---
+++ lesson4/lesson4.js
@@ -0,0 +1,87 @@
+// // let message = 'Hello!';
+// // let name = 'My name is John.';
+// // let age = 'I am 30 years old.';
+// // message = `${message} ${name} ${age}`;
+// // console.log(message);
+
+// // let message = 'Hello!';
+// // let name = 'My name is John.';
+// // let age = 'I am 30 years old.';
+
+// // console.log(`${message} ${name} ${age}`);
+
+// // let sum = 5 + 3;
+
+// // console.log(sum);
+// // console.log(10 + 20);
+
+// // let result = 10 - 4;
+// // console.log(result);
+
+// // let result2 = 20 / 5;
+// // console.log(result2);
+
+// // let result3 = 3 * 7;
+// // console.log(result3);
+
+// let result4 = 10 + 5 * 20;
+// console.log(result4);
+
+// let number = 15;
+
+// console.log(number % 4);
+
+// let a = 10;
+// a++;
+// console.log(a);
+// a--;
+// console.log(a);
+
+// let a = 5;
+// let b = a++;
+// console.log(a);
+// console.log(b);
+
+// let a = 5;
+// let b = ++a
+// console.log(a);
+// console.log(b);
+
+// console.log(5 == '5');
+
+// console.log(5 === '5');
+
+// console.log(5 != '5');
+// console.log(5 !=='5');
+
+// const x = 0.1;
+// const y = 0.2;
+
+// const sum = x + y;
+// console.log(sum);
+
+// console.log(0.17 + 0.24); // 0.41000000000000003
+// console.log((0.17 + 0.24).toFixed(4)); // 0.41
+
+// console.log(Math.PI);
+
+// let x = 5.19
+// console.log(Math.round(x));
+// console.log(Math.floor(x));
+// console.log(Math.ceil(x));
+
+// console.log(Math.floor(Math.random() * 100));
+
+//Types
+
+// console.log(parseInt('55.55'));
+// console.log(parseFloat('455.454'));
+// console.log(Number('455454'));
+// console.log(+('455454'));
+
+// let numberA = 100;
+// let numberB = 200;
+// let numberC = 300;
+
+// console.log(String(numberA) + String(numberB) + String(numberC));
+// console.log(numberB.toString());
