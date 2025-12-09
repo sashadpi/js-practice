f4f8f52f3db940da1a666c4b9c4b12a4a686f94c
*Sasha* <*sashadpi68@gmail.com*>
2025-12-09 11:22:39 +0200

    Add a blank line for better readability in task3.js

--- lesson4/task3.js
+++ lesson4/task3.js
@@ -2,6 +2,7 @@ let min = 1;
 let max = 100;
 let randomAge = Math.floor(Math.random() * (max - min + 1)) + min;
 let checkAge = (randomAge >= 18);
+
 console.log('Your age is ' + randomAge);
 console.log(`You are an adult.` + checkAge);
 
