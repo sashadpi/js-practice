c35da76dd752bbe3fe58e86e6136cf404f2f596f
*Sasha* <*sashadpi68@gmail.com*>
2025-12-15 21:29:32 +0200

    feat: add functions for area calculation, adult check, and order validation in lesson 6

---
+++ lesson6/functions.js
@@ -0,0 +1,24 @@
+// function sayHello () {
+// 	console.log('Hello!');
+// 	console.log('Hello World!');
+// }
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+// function calculateSum(num1, num2) {
+// 	const result = num1 + num2;
+// 	console.log(`${num1} + ${num2} = ${result}`);
+// }
+// calculateSum(10, 20);
+// calculateSum(10);
+// calculateSum(10, 40, 40);
+
+
+function getSum(num1, num2) {
+	const result = num1 + num2;
+	return result;
+}
+
+console.log(getSum(10, 50));
---
+++ lesson6/task2.js
@@ -0,0 +1,5 @@
+function adult(age) {
+    return age >= 18;
+}
+console.log(adult(25));
+console.log(adult(15));
