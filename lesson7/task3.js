fbee47ad87b56883c103df830727c012b835a3f1
*Sasha* <*sashadpi68@gmail.com*>
2025-12-21 14:40:49 +0200

    feat: add functions for number handling, countdown, and error handling in lesson 7

---
+++ lesson7/task3.js
@@ -0,0 +1,33 @@
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+        throw new Error('Ділення на нуль неможливе');
+    } if (typeof numerator !== 'number' || typeof denominator !== 'number') {
+        throw new Error('Обидва аргументи повинні бути числами');
+    }
+    return numerator / denominator;
+}
+
+try {
+    const result = divide(14, 2);
+    console.log(result);
+} catch (error) {
+    console.error(error.message);
+} finally {
+    console.log('Робота завершена');
+}
+
+try {
+    divide(10, 0)
+} catch (error) {
+    console.log(error.message);
+} finally {
+    console.log('Робота завершена');
+}
+
+try {
+    divide('10', 2)
+} catch (error) {
+    console.log(error.message);
+} finally {
+    console.log('Робота завершена');
+}
